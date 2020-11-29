e0232e9b1b330f4da2b4f6f9de5afa26598c8028
ILJA <iljakristafenko@gmail.com>
2020-11-30 00:44:39 +0200

    practicNum1

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 const butJs = document.querySelector("button");
 
-butJs.addEventListener("click", () => {
-    alert("cklick");
-});
+// butJs.addEventListener("click", () => {
+//     alert("cklick");
+// });
 
+// butJs.addEventListener("click", () => {       //! -------- click listener 
+//     alert("second slick");
+// });
+
+butJs.addEventListener("mouseenter", () => {        //! ---------- mouse hover listener
+    console.log("Hover-mouse");
+});
