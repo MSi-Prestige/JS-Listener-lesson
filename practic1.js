0d93d31df84e3aaada96b412691698154b6721f7
ILJA <iljakristafenko@gmail.com>
2020-11-30 22:43:44 +0200

    paractic1-2-3

--- practic1.js
+++ practic1.js
@@ -11,12 +11,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),   //! dop ""  << dlja winExplorer 11 vers defalult
-b = prompt("На сколько оцените его?", ""),
-c = prompt("Один из последних просмотренных фильмов?", ""),
-d = prompt("На сколько оцените его?", "");
+for ( let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;   //! Kljuch [a] ---- b = znachenie 
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
-console.log(numberOfFilms);
+     if ( a != "" && b != "" && a != null && b != null && a.length < 20 && b.length < 3){
+          personalMovieDB.movies[a] = b;  //! ---------- Kljuch [a] ---- b = znachenie 
+          console.log("data add in BD");
+     } else {
+         console.log("error");
+         i--;
+     }
+}
+
+
+
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),   //! dop ""  << dlja winExplorer 11 vers defalult
+// b = prompt("На сколько оцените его?", ""),
+// c = prompt("Один из последних просмотренных фильмов?", ""),
+// d = prompt("На сколько оцените его?", "");
+
+
